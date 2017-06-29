9974100c148b79cde8984fa91c79b58f8812932d
Eclipse95 <alexis.bitaillou@etu.univ-nantes.fr>
2017-06-29 12:05:18 +0200

    fix problem discuss in #2

--- lazyDroid.sh
+++ lazyDroid.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-# lazzyDroid
+#!/usr/bin/env bash
+# lazyDroid
 # 2017 By Dani Martinez (@dan1t0) / NCCGroup
 
 VERSION="0.4"
@@ -216,7 +216,7 @@ function set_something {
 function smartLog {
     echo -n "Execute the app and enter the name: "
     read packageName
-    ${ADB} shell "ps | grep $packageName" > /tmp/pids
+    ${ADB} shell ps | grep $packageName > /tmp/pids
     PIDS="$(wc -l /tmp/pids | cut -d" " -f1)"
     DATE=$(date +"%Y%m%d%H%M%S")
     FOLDER=$PWD
